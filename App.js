126cd6ca5b78b2479552b206b570f1ddac2a16b6
adnaan2328 <59410944+adnaan2328@users.noreply.github.com>
2021-09-26 22:59:19 +0530

    reanimated cheatsheet

--- App.js
+++ App.js
@@ -1,95 +1,132 @@
-import React from 'react';
-import {View, Text, Button, useWindowDimensions, Alert} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  useWindowDimensions,
+  Alert,
+  ScrollView,
+} from 'react-native';
 import {PanGestureHandler} from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedGestureHandler,
   useSharedValue,
   useAnimatedStyle,
   withSpring,
+  interpolate,
+  withTiming,
+  withDelay,
+  withRepeat,
+  withSequence,
+  useAnimatedScrollHandler,
+  interpolateColor,
 } from 'react-native-reanimated';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
-  const dimensions = useWindowDimensions();
+  // shared values which works on UI thread
+  const sharedOpacity = useSharedValue(0);
 
-  const topSharedValue = useSharedValue(dimensions.height);
+  const touchX = useSharedValue(0);
+  const touchY = useSharedValue(0);
 
-  // this return the style based on shared value
-  const reanimatedTopStyle = useAnimatedStyle(() => {
+  const scrollY = useSharedValue(0);
+
+  useEffect(() => {
+    // updating shared value after 1 second
+    // sharedOpacity.value = withTiming(1, {
+    //   duration: 1000,
+    // });
+    // spring animation
+    // sharedOpacity.value = withSpring(1);
+    // with delay
+    // sharedOpacity.value = withDelay(5000, withSpring(1));
+    //with repeat
+    //sharedOpacity.value = withRepeat(withSpring(1), 4, true);
+    //with sequence
+    // sharedOpacity.value = withSequence(
+    //   withSpring(1),
+    //   withSpring(0.5),
+    //   withSpring(0),
+    // );
+  }, []);
+
+  // for scroll behaviour
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  // for gesture behaviour
+  const gestureHandler = useAnimatedGestureHandler({
+    onActive(event) {
+      (touchX.value = event.translationX), (touchY.value = event.translationY);
+    },
+    onEnd(event) {
+      (touchX.value = withSpring(0)), (touchY.value = withSpring(0));
+    },
+  });
+
+  // const sharedStyle = useAnimatedStyle(() => {
+  //   return {
+  //     opacity: sharedOpacity.value,
+  //     transform: [
+  //       {
+  //         scale: interpolate(sharedOpacity.value, [0, 1], [1, 3]),
+  //       },
+  //     ],
+  //   };
+  // });
+
+  const sharedStyle = useAnimatedStyle(() => {
     return {
-      top: withSpring(topSharedValue.value, {
-        damping: 300,
-        overshootClamping: true,
-        restDisplacementThreshold: 0.1,
-        restSpeedThreshold: 0.1,
-        stiffness: 500,
-      }),
+      transform: [
+        {
+          translateX: touchX.value,
+        },
+        {
+          translateY: touchY.value,
+        },
+      ],
     };
   });
 
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart(_, context) {
-      context.topStartValue = topSharedValue.value;
-    },
-
-    onActive(event, context) {
-      topSharedValue.value = context.topStartValue + event.translationY;
-    },
-
-    onEnd() {
-      if (topSharedValue.value > dimensions.height / 2 + 100) {
-        topSharedValue.value = dimensions.height;
-      } else {
-        topSharedValue.value = dimensions.height / 2;
-      }
-    },
+  const scrollViewAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(
+        touchY.value,
+        [-50, 0, 50],
+        ['rgb(10,10,10)', 'rgba(255,255,255)', 'rgb(10,10,10)'],
+      ),
+    };
   });
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Button
-          title={'Open sheet'}
-          onPress={() => {
-            topSharedValue.value = withSpring(dimensions.height / 2, {
-              damping: 300,
-              overshootClamping: true,
-              restDisplacementThreshold: 0.1,
-              restSpeedThreshold: 0.1,
-              stiffness: 500,
-            });
-          }}
-        />
-      </View>
-      <PanGestureHandler onGestureEvent={gestureHandler}>
-        <Animated.View
-          style={[
-            {
-              position: 'absolute',
-              left: 0,
-              right: 0,
-              bottom: 0,
-              backgroundColor: 'white',
-              borderTopRightRadius: 20,
-              borderTopLeftRadius: 20,
-              shadowColor: '#000',
-              shadowOffset: {
-                width: 0,
-                height: 2,
-              },
-              shadowOpacity: 0.25,
-              shadowRadius: 2.84,
-              elevation: 5,
-              padding: 20,
-              justifyContent: 'center',
-              alignItems: 'center',
-            },
-            reanimatedTopStyle,
-          ]}>
-          <Text>Sheet</Text>
-        </Animated.View>
-      </PanGestureHandler>
-    </GestureHandlerRootView>
+    <Animated.ScrollView
+      onScroll={scrollHandler}
+      scrollEventThrottle={16}
+      style={[scrollViewAnimatedStyle]}
+      contentContainerStyle={{
+        height: '100%',
+        justifyContent: 'center',
+      }}>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <PanGestureHandler onGestureEvent={gestureHandler}>
+            <Animated.View
+              style={[
+                {
+                  height: 50,
+                  width: 50,
+                  borderRadius: 25,
+                  backgroundColor: 'red',
+                },
+                sharedStyle,
+              ]}
+            />
+          </PanGestureHandler>
+        </View>
+      </GestureHandlerRootView>
+    </Animated.ScrollView>
   );
 };
 
