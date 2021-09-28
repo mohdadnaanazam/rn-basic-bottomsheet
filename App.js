2309d0c51ed2a78fb8a92b6536afda8247799676
adnaan2328 <59410944+adnaan2328@users.noreply.github.com>
2021-09-28 18:38:21 +0530

    firebase push notification added

--- App.js
+++ App.js
@@ -1,133 +1,70 @@
 import React, {useEffect} from 'react';
-import {
-  View,
-  Text,
-  Button,
-  useWindowDimensions,
-  Alert,
-  ScrollView,
-} from 'react-native';
-import {PanGestureHandler} from 'react-native-gesture-handler';
-import Animated, {
-  useAnimatedGestureHandler,
-  useSharedValue,
-  useAnimatedStyle,
-  withSpring,
-  interpolate,
-  withTiming,
-  withDelay,
-  withRepeat,
-  withSequence,
-  useAnimatedScrollHandler,
-  interpolateColor,
-} from 'react-native-reanimated';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {View, Button, Alert} from 'react-native';
+import database from '@react-native-firebase/database';
+import messaging from '@react-native-firebase/messaging';
+import notifee, {EventType} from '@notifee/react-native';
 
-const App = () => {
-  // shared values which works on UI thread
-  const sharedOpacity = useSharedValue(0);
+function Screen() {
+  useEffect(async () => {
+    await messaging().registerDeviceForRemoteMessages();
 
-  const touchX = useSharedValue(0);
-  const touchY = useSharedValue(0);
-
-  const scrollY = useSharedValue(0);
-
-  useEffect(() => {
-    // updating shared value after 1 second
-    // sharedOpacity.value = withTiming(1, {
-    //   duration: 1000,
-    // });
-    // spring animation
-    // sharedOpacity.value = withSpring(1);
-    // with delay
-    // sharedOpacity.value = withDelay(5000, withSpring(1));
-    //with repeat
-    //sharedOpacity.value = withRepeat(withSpring(1), 4, true);
-    //with sequence
-    // sharedOpacity.value = withSequence(
-    //   withSpring(1),
-    //   withSpring(0.5),
-    //   withSpring(0),
-    // );
+    // Get the token
+    const token = await messaging().getToken();
+    console.log(token);
   }, []);
 
-  // for scroll behaviour
-  const scrollHandler = useAnimatedScrollHandler(event => {
-    scrollY.value = event.contentOffset.y;
-  });
-
-  // for gesture behaviour
-  const gestureHandler = useAnimatedGestureHandler({
-    onActive(event) {
-      (touchX.value = event.translationX), (touchY.value = event.translationY);
-    },
-    onEnd(event) {
-      (touchX.value = withSpring(0)), (touchY.value = withSpring(0));
-    },
-  });
-
-  // const sharedStyle = useAnimatedStyle(() => {
-  //   return {
-  //     opacity: sharedOpacity.value,
-  //     transform: [
-  //       {
-  //         scale: interpolate(sharedOpacity.value, [0, 1], [1, 3]),
-  //       },
-  //     ],
-  //   };
-  // });
-
-  const sharedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        {
-          translateX: touchX.value,
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      // Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      await notifee.displayNotification({
+        title: 'Notification Title',
+        body: 'Main body content of the notification',
+        android: {
+          channelId: 'someId',
         },
-        {
-          translateY: touchY.value,
-        },
-      ],
-    };
-  });
+      });
+    });
 
-  const scrollViewAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      backgroundColor: interpolateColor(
-        touchY.value,
-        [-50, 0, 50],
-        ['rgb(10,10,10)', 'rgba(255,255,255)', 'rgb(10,10,10)'],
-      ),
-    };
-  });
+    return unsubscribe;
+  }, []);
+
+  async function bootstrap() {
+    const initialNotification = await notifee.getInitialNotification();
+
+    if (initialNotification) {
+      console.log(
+        'Notification caused application to open',
+        initialNotification.notification,
+      );
+      console.log(
+        'Press action used to open the app',
+        initialNotification.pressAction,
+      );
+    }
+  }
+
+  useEffect(() => {
+    bootstrap()
+      .then(() => console.log('SUCCESS :'))
+      .catch(console.error);
+  }, []);
 
   return (
-    <Animated.ScrollView
-      onScroll={scrollHandler}
-      scrollEventThrottle={16}
-      style={[scrollViewAnimatedStyle]}
-      contentContainerStyle={{
-        height: '100%',
-        justifyContent: 'center',
-      }}>
-      <GestureHandlerRootView style={{flex: 1}}>
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <PanGestureHandler onGestureEvent={gestureHandler}>
-            <Animated.View
-              style={[
-                {
-                  height: 50,
-                  width: 50,
-                  borderRadius: 25,
-                  backgroundColor: 'red',
-                },
-                sharedStyle,
-              ]}
-            />
-          </PanGestureHandler>
-        </View>
-      </GestureHandlerRootView>
-    </Animated.ScrollView>
+    <View>
+      <Button
+        title="Display Notification"
+        onPress={async () => {
+          await notifee.displayNotification({
+            title: 'Notification Title',
+            body: 'Main body content of the notification',
+            android: {
+              channelId: 'someId',
+            },
+          });
+        }}
+      />
+    </View>
   );
-};
+}
 
-export default App;
+export default Screen;
--- index.js
+++ index.js
@@ -3,10 +3,43 @@
  */
 
 import 'react-native-gesture-handler';
-
-import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
-import {AppRegistry, Platform, NativeModules} from 'react-native';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import notifee, {AndroidImportance, EventType} from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 
 AppRegistry.registerComponent(appName, () => App);
+
+const onTapNotificationInClosed = async () => {
+  setTimeout(async () => {
+    const notifData = await messaging().getInitialNotification();
+    console.log(notifData, 'FIRST MEAN KILLED STATE');
+  }, 4000);
+};
+
+const onTapNotificationInBackground = async () => {
+  messaging().onNotificationOpenedApp(messages => {
+    if (messages) {
+      console.log(messages, 'SECOND SWITCHED TO ANOTHER SCREEN ');
+    }
+  });
+};
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
+
+notifee.createChannel({
+  id: 'someId',
+  name: 'some name',
+  lights: false,
+  vibration: true,
+  importance: AndroidImportance.DEFAULT,
+});
+console.log(
+  '\n\n\n FROM INDEX',
+  'Background notification ***********************',
+);
+onTapNotificationInClosed();
+onTapNotificationInBackground();
